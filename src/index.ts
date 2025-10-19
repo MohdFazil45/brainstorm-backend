8ede7fa49d72483ac438de6b4bbec2a1e10999bb
MohdFazil45 <mdfazil846@gmail.com>
2025-10-19 19:15:00 +0530

    added correct cors

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-  origin: "https://your-frontend.netlify.app", // Replace with your Netlify URL
+  origin: "https://fazil-brainstorm.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 }));
